2fcaf9de612ba8ae8d1ed9db06b6ef33bb63660f
Luan Henrique <luan.mar76@gmail.com>
2025-08-31 22:18:18 -0300

    aprendendo rotas

--- server/bin/Routes.dart
+++ server/bin/Routes.dart
@@ -2,11 +2,22 @@ import 'package:shelf/shelf.dart';
 import 'package:shelf_router/shelf_router.dart';
 
 class Rotas {
-  Handler? get handler {
-    final rout = Router();
+  Handler get handler {
+    final router = Router();
 
-    rout.get("/", (Request request) {
+    // PARA CADA ROTA TEM QUE ENTRA COMO PARAM UM REQ E RETORNAR UM RESPONSE
+
+    //
+    router.get("/", (Request request) {
       return Response(200, body: "primeira rota");
     });
+
+    router.get('/ola/<id>', (Request req) {
+      var UserID = req.params['id'];
+
+      return Response.ok("Ola mundo SR. ${UserID}");
+    });
+
+    return router;
   }
 }
--- server/bin/server.dart
+++ server/bin/server.dart
@@ -6,11 +6,7 @@ import 'Routes.dart';
 void main() async {
   Rotas rout = Rotas();
 
-  final serv = await shelfio.serve(
-    (request) => Response(200, body: 'ok'),
-    'localhost',
-    8080,
-  );
+  final server = await shelfio.serve(rout.handler, 'localhost', 8080);
 
   print("serv start 8080");
 }
