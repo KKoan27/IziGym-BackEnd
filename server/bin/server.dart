e6674feaf3199309ebcde22d2c9a6c89cf50fcf9
Luan Henrique <luan.mar76@gmail.com>
2025-09-11 11:00:30 -0300

    Corrigindo o servedart

--- server/bin/server.dart
+++ server/bin/server.dart
@@ -6,11 +6,7 @@ import 'Routes.dart';
 void main() async {
   Endpoint rout = Endpoint();
 
-<<<<<<< HEAD
   final serv = await shelfio.serve(rout.handler, 'localhost', 8080);
-=======
-  final server = await shelfio.serve(rout.handler, 'localhost', 8080);
->>>>>>> cf80dce0116962332630cf364acee815bfde2b0d
 
   print("serv start 8080");
 }
