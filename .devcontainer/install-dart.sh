c096be31e26da854504f2c7329b432d6c430bda8
Luan Henrique <128068334+KKoan27@users.noreply.github.com>
2025-09-18 01:42:34 +0000

    17/09

--- .devcontainer/install-dart.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-sudo apt update && sudo apt install -y apt-transport-https
-sudo sh -c 'wget -qO- https://dl-ssl.google.com/linux/linux_signing_key.pub | gpg --dearmor > /usr/share/keyrings/dart-archive-keyring.gpg'
-sudo sh -c 'echo "deb [signed-by=/usr/share/keyrings/dart-archive-keyring.gpg] https://storage.googleapis.com/download.dartlang.org/linux/debian stable main" > /etc/apt/sources.list.d/dart_stable.list'
-
-sudo apt update && sudo apt install -y dart
-echo 'export PATH="$PATH:/usr/lib/dart/bin"' >> ~/.bashrc
