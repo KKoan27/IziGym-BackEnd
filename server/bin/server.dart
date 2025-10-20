80185842b999e40c1a12af51baa9d43b6035752c
Luan Henrique <128068334+KKoan27@users.noreply.github.com>
2025-10-20 13:36:14 -0300

    Refactor server to use dynamic address and port
    Updated server to read address and port from environment variables dynamically.

--- server/bin/server.dart
+++ server/bin/server.dart
@@ -1,23 +1,27 @@
+import 'dart:io';
 import 'package:shelf/shelf.dart';
-import 'package:shelf/shelf_io.dart' as shelfio;
+import 'package.shelf/shelf_io.dart' as shelfio;
 import 'Routes.dart';
 import 'Utilitys/custom_env.dart';
-import 'dart:io'; 
 
 void main() async {
   Endpoint rout = Endpoint();
   
-var handler = Pipeline()
+  var handler = Pipeline()
       .addMiddleware(logRequests())
       .addHandler(rout.handler);
 
-  final address = '0.0.0.0';
-  final platformPort = Platform.environment['PORT'];
+  // Lendo o ADDRESS dinamicamente
+  final platformAddress = Platform.environment['SERVER_ADDRESS'];
+  final address = platformAddress ?? await Customenv.get<String>(key: 'SERVER_ADDRESS');
+
+  // Lendo a PORT dinamicamente
+  final platformPort = Platform.environment['SERVER_PORT'];
   final port = platformPort != null
       ? int.parse(platformPort)
       : await Customenv.get<int>(key: 'SERVER_PORT');
 
-  await shelfio.serve(handler, address, port);
+  final server = await shelfio.serve(handler, address, port);
 
-  print("serv start $address : $port ");
+  print("🚀 Servidor iniciado em http://${server.address.host}:${server.port}");
 }
