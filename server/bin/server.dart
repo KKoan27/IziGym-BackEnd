27b068f674b15d8430c81f5b2deafd26c093788d
Luan Henrique <128068334+KKoan27@users.noreply.github.com>
2026-07-16 18:30:11 -0300

    refactor: Infra - server.dart

--- server/bin/server.dart
+++ server/bin/server.dart
@@ -1,4 +1,3 @@
-import 'dart:convert';
 import 'dart:io';
 import 'package:mongo_dart/mongo_dart.dart';
 import 'package:server/Controllers/ExercicioController.dart';
@@ -13,27 +12,24 @@ import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart' as shelfio;
 import 'package:server/Router.dart';
 import 'package:server/Utilitys/custom_env.dart';
-import 'package:shelf_cors_headers/shelf_cors_headers.dart' as cors;
 import 'package:server/Controllers/UserController.dart';
-import 'package:server/Controllers/ExercicioController.dart';
 import 'package:server/Services/ExercicioService.dart';
-import 'package:server/data/DAO/ExercicioDAO.dart';
+import 'package:server/Utilitys/Middlewares.dart';
 
 void main() async {
+
+  //Conexão com o BD
   Db db = await MongoConn.database;
+
   //DAO's
   TreinoDAO treinodao  = TreinoDAO(db);
   ExercicioDAO exerciciodao = ExercicioDAO(db);
   UserDAO userdao = UserDAO(db);
 
-
   //Services
   UserService userservice = UserService(userdao);
   ExercicioService exercicioservice = ExercicioService(exerciciodao);
-
   TreinoService treinoservice = TreinoService(treinodao);
-
-
 
   //Controllers
   Treinoscontroller treinoscontroller = Treinoscontroller(treinoservice);
@@ -46,16 +42,9 @@ void main() async {
     treinoctrl: treinoscontroller,
   );
 
-  const corsHeaders = {
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-    'Access-Control-Allow-Headers': 'Origin, Content-Type',
-  };
-
   var handler = Pipeline()
       .addMiddleware(logRequests())
-      .addMiddleware(cors.corsHeaders(headers: corsHeaders))
-      .addMiddleware(standardResponseMiddleware(corsHeaders))
+      .addMiddleware(standardRespondeMiddleware())
       .addHandler(rout.handler);
 
   // Lendo o ADDRESS dinamicamente
@@ -72,68 +61,4 @@ void main() async {
   final server = await shelfio.serve(handler, address, port);
 
   print("🚀 Servidor iniciado em http://${server.address.host}:${server.port}");
-}
-
-// Organizar essas Middleware
-
-// Nome da função do nosso middleware
-Middleware standardResponseMiddleware(Map<String, String> corsHeaders) {
-  // A estrutura padrão de um middleware: recebe um handler e retorna outro.
-  return (Handler innerHandler) {
-    // Este é o novo handler que será executado.
-    // Usamos 'async' porque vamos 'await' a resposta do handler interno.
-    return (Request request) async {
-      // evita quebrar o pré-flight do CORS
-      if (request.method == 'OPTIONS') {
-        return Response.ok('', headers: {...corsHeaders});
-      }
-
-      try {
-        final originalResponse = await innerHandler(request);
-        final originalBodyString = await originalResponse.readAsString();
-
-        dynamic responseBody;
-
-        if (originalBodyString.isNotEmpty) {
-          try {
-            responseBody = jsonDecode(originalBodyString);
-          } catch (_) {
-            responseBody = originalBodyString;
-          }
-        } else {
-          responseBody = {};
-        }
-
-        final standardPayload = {
-          'methodRequest': request.method,
-          'statusCode': originalResponse.statusCode,
-          'response': responseBody,
-        };
-
-        return originalResponse.change(
-          body: jsonEncode(standardPayload),
-          headers: {
-            ...originalResponse.headers,
-            'Content-Type': 'application/json',
-          },
-        );
-      } catch (e, s) {
-
-        print('erro: $e \n\n $s');
-        final errorPayload = {
-          'methodRequest': request.method,
-          'statusCode': 500,
-          'response': {
-            'error': 'Ocorreu um erro interno no servidor.',
-            'details': e.toString(),
-          },
-        };
-
-        return Response.internalServerError(
-          body: jsonEncode(errorPayload),
-          headers: {'Content-Type': 'application/json'},
-        );
-      }
-    };
-  };
 }
